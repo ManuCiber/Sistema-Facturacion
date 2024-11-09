89545b9e4d41cd36901ba4cacd46bea429f253b5
Manuel-Program <86160611+ManuCiber@users.noreply.github.com>
2024-11-09 09:37:50 -0400

    Archivo Modificado

--- backend/routes/Cliente.js
+++ backend/routes/Cliente.js
@@ -25,13 +25,32 @@ router.get('/', async (req, res) => {
     
 })
 
+/*Actualizar el Cliente*/
 
+ router.patch('/:id',async (req, res) => {
+     try {
+        const cliente = await Cliente.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        if(!cliente){
+            return res.status(404).send({message: 'Cliente no encontrado'})
+        }
+        res.send(cliente)
+     } catch (error) {
+        res.status(400).send({message: 'Error al actualizar cliente'})
+     }
+})
 
-// router.patch('/:id',async (req, res) => {
-//     try {
-        
-//     } catch (error) {
-        
-//     }
-// })
+/*Para eliminar un cliente*/
 
+router.delete('/:id', async (req, res) => {
+    try {
+        const cliente = await Cliente.findOneAndDelete(req.params.id)
+        if(!cliente){
+            return res.status(404).send({message: 'Cliente no encontrado'})
+        }
+        res.send(cliente)
+    } catch (error) {
+        res.status(500).send({message: 'Error al eliminar Cliente'})
+    }
+})
+
+module.exports = router
